ea7dbc923b4b6ebddd301235792f188c54e2dd96
Tim Bergerhofer <tbergerhofer@gmail.com>
2013-12-24 10:03:21 -0600

    Here are the changes that updated front-end forms for authentication system
    - Addeded simpleform
    - Updated forms
    - Added login/lgout/edit profile links

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,7 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :name
+  attr_accessible :content, :user_id
+  	belongs_to :user
 end
+ def full_name
+ 	first_name + " " + last_name
+ end
